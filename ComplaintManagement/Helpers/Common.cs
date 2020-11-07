0c7dc441d8035911d80406159a4234de71c9aa75
ketansali <61194857+ketansali@users.noreply.github.com>
2020-11-07 18:53:03 +0530

    added temp file

--- ComplaintManagement/Helpers/Common.cs
+++ ComplaintManagement/Helpers/Common.cs
@@ -1,11 +1,10 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-
-namespace ComplaintManagement.Helpers
+﻿namespace ComplaintManagement.Helpers
 {
     public class Common
     {
+        public void Sample()
+        {
+
+        }
     }
 }
