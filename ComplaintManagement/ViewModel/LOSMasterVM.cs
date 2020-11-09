fb3341236da5ba7e5f61eb0c34d2e8842c63041b
ketansali <61194857+ketansali@users.noreply.github.com>
2020-11-09 15:30:32 +0530

    added los

---
+++ ComplaintManagement/ViewModel/LOSMasterVM.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ComplaintManagement.ViewModel
+{
+    public class LOSMasterVM
+    {
+        public int Id { get; set; }
+        public string LOSName { get; set; }
+        public int SBUId { get; set; }
+        public int SubSBUId { get; set; }
+        public int CompetencyId { get; set; }
+        public bool Status { get; set; }
+        public int UserId { get; set; }
+        public System.DateTime CreatedDate { get; set; }
+        public bool IsActive { get; set; }
+        public Nullable<System.DateTime> UpdatedDate { get; set; }
+    }
+}
